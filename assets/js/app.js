7197df2684ebaa6cb6cfc418dc74668cf0d810a1
rasheed <olanrewajurasheedopeyemi@gmail.com>
2022-06-25 17:07:01 +0100

    edited

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,12 @@
-// creation of eventListeners
+const tweetList = document.getElementById("tweet-list");
 
+// creation of eventListeners
 eventListeners();
 
 function eventListeners() {
   document.querySelector(".button").addEventListener("click", LoadBtnFnc);
-
   document.getElementById("tweet-list").addEventListener("click", RemoveBtnFnc);
+  document.addEventListener("DOMContentLoaded", getFromLocalStorage);
 }
 
 // Function to loadtweet
@@ -26,16 +27,24 @@ function LoadBtnFnc(e) {
 
   //appending clearButton to tweet
   li.appendChild(clearBtn);
-  ftweet = document.getElementById("tweet-list").appendChild(li);
+  tweetList.appendChild(li);
 
   //Local Storage for tweets
-  addLocalStorage();
+  addLocalStorage(tweet);
+
+  //Alert for new tweet added
+  alert("New tweet added!")
+
+  // clean up the text area 
+  this.reset();
 }
 
 function RemoveBtnFnc(e) {
   if (e.target.classList.contains("remove-tweet")) {
     e.target.parentElement.remove();
   }
+
+  removeTweetLocalStorage(e.target.parentElement.textContent);
 }
 
 function addLocalStorage(tweet) {
@@ -44,17 +53,56 @@ function addLocalStorage(tweet) {
   // Add tweets into the array
   tweets.push(tweet);
 
-  localStorage.setItem("tweet", JSON.stringify( tweets ));
+  localStorage.setItem("tweets", JSON.stringify(tweets));
 }
 
 function getTweetsLocalStorage() {
   let tweets;
-  const tweetLS = localStorage.getItem("tweets");
+  const tweetsLS = localStorage.getItem("tweets");
 
-  if (tweetLS === null) {
+  if (tweetsLS === null) {
     tweets = [];
   } else {
-    tweets = JSON.parse(tweetLS);
+    tweets = JSON.parse(tweetsLS);
   }
   return tweets;
+}
+
+//printing data from local storage
+function getFromLocalStorage() {
+  let tweetsLS = getTweetsLocalStorage();
+
+  //looping through
+  tweetsLS.forEach(function (tweet) {
+    // adding clear button
+    clearBtn = document.createElement("a");
+    clearBtn.classList = "remove-tweet";
+    clearBtn.textContent = "X";
+
+    //fetching text value
+    li = document.createElement("li");
+    li.textContent = tweet;
+
+    //appending clearButton to tweet
+    li.appendChild(clearBtn);
+    tweetList.appendChild(li);
+  });
+}
+
+// Remove tweet from local storage
+function removeTweetLocalStorage(tweet) {
+  let tweets = getTweetsLocalStorage();
+
+  const tweetDelete = tweet.substring(0, tweet.length - 1);
+
+  tweets.forEach(function (tweetLS, index) {
+    if (tweetDelete === tweetLS) {
+      tweets.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem("tweets", JSON.stringify(tweets));
+
+  //alert for tweet removal
+  alert("Tweet deleted!")
 }
